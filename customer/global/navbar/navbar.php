e9f7cce244c5baa5cd67ea63f5b5071299e73fd5
SalmaAsaadd <154117968+SalmaAsaadd@users.noreply.github.com>
2025-06-06 13:15:50 +0300

    Update navbar.php

--- customer/global/navbar/navbar.php
+++ customer/global/navbar/navbar.php
@@ -30,10 +30,6 @@ $user_id = $_SESSION['user_id'];
                 <span class="nav-icon meals"></span>
                 <span class="nav-text">Track my Order</span>
             </a>
-              <a href="\NEW-TODAYS-MEAL\customer\Show_Caterers\index.php" class="nav-item">
-                <span class="nav-icon order"></span>
-                <span class="nav-text">Start Ordering</span>
-            </a>
             <a href="\NEW-TODAYS-MEAL\customer\support\support.php" class="nav-item">
                 <span class="nav-icon support"></span>
                 <span class="nav-text">Support</span>
@@ -61,7 +57,7 @@ $user_id = $_SESSION['user_id'];
             <img src="https://img.icons8.com/windows/32/key.png" class="settings-item-icon" alt="Change Password">
             <span>Change Password</span>
         </a>
-        <a href="#" class="settings-item">
+        <a href="..\Register&Login\login.php" class="settings-item">
             <svg class="settings-item-icon" viewBox="0 0 24 24">
                 <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"/>
             </svg>
